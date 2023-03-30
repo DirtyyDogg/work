2c4df51aa00ea0d268ffcb8211e7d311454b8f07
DirtyyDogg500 <96761238+DirtyyDogg500@users.noreply.github.com>
2023-03-30 16:55:50 +0200

    u

--- if((get_lval(PS4_LEFT)&&get_ptime(PS4_LE.cs
+++ if((get_lval(PS4_LEFT)&&get_ptime(PS4_LE.cs
@@ -10,4 +10,4 @@ if(MENU_SECTION_RUNNING)
     if(event_press(PS4_CIRCLE))PLACE_ON_MENU = PLACE_ON_MENU - 1; 
     if((PLACE_ON_MENU<0)||(get_lval(PS4_CIRCLE)&&get_ptime(PS4_CIRCLE)>800)){ SNIPER_RIFLE=TRUE; PLACE_ON_MENU=0; MENU_SECTION_RUNNING=FALSE;  } 
   }
-    else
+    // else
